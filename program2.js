562693ce451c17ac86cf1c9a6b159cc2afa43d53
Choudhary387 <128511188+Choudhary387@users.noreply.github.com>
2024-02-23 07:59:29 +0000

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,9 +2,22 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s){
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function consistsOnlyOfZeros(s) {
 
-};
-
-
-module.exports={romanToInt}
+    if (s.length === 0) {
+      return false;
+    }
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] !== '0') {
+        return false;
+      }
+    }
+    return true;
+  }
+console.log(consistsOnlyOfZeros("0")); 
+console.log(consistsOnlyOfZeros("000")); 
+console.log(consistsOnlyOfZeros("")); 
