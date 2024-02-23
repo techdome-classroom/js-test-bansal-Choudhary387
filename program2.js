7b04f7bff50a0e6252bd49afa67c07d76b93f9b1
Choudhary387 <128511188+Choudhary387@users.noreply.github.com>
2024-02-23 13:10:43 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -2,28 +2,9 @@
  * @param {string} s
  * @return {number}
  */
-function.romanNumber(s){
- const romanMap = new Map([
-    ['I', 1],
-    ['V', 5],
-    ['X', 10],
-    ['L', 50],
-    ['C', 100],
-    ['D', 500],
-    ['M', 1000]
-  ]);
-  let result = 0; 
-  for (let i = 0; i < s.length; i++) {
-    const currentVal = romanMap.get(s[i]);
-    const nextVal = romanMap.get(s[i + 1]);
-    if (nextVal && currentVal < nextVal) {
-      result -= currentVal;
-    } else {
-      result += currentVal;
-    }
-  }
-  return result;
-}
-console.log(romanToInt("III"));
-console.log(romanToInt("LVIII")); 
-console.log(romanToInt("MCMXCIV"));
+var romanToInt = function(s){
+
+};
+
+
+module.exports={romanToInt}
