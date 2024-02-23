4c0fb1da76863466adfb46d40db7894db4481906
Choudhary387 <128511188+Choudhary387@users.noreply.github.com>
2024-02-23 13:11:57 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,15 +1,10 @@
-function consistsOnlyOfZeros(s) {
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s){
 
-    if (s.length === 0) {
-      return false;
-    }
-    for (let i = 0; i < s.length; i++) {
-      if (s[i] !== '0') {
-        return false;
-      }
-    }
-    return true;
-  }
-console.log(consistsOnlyOfZeros("0")); 
-console.log(consistsOnlyOfZeros("000")); 
-console.log(consistsOnlyOfZeros("")); 
+};
+
+
+module.exports={isValid}
